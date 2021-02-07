45f64bf6aa617fe887becc3146c9a77a4dc4a480
jona <moreirajonatan1983@gmail.com>
2021-02-07 02:52:54 -0300

    Modifico clase principal del servicio y agrego controllador con mapping para verficar sy funcionamiento.

--- src/main/java/com/ma/pedidos/MainApp.java
+++ src/main/java/com/ma/pedidos/MainApp.java
@@ -2,8 +2,10 @@ package com.ma.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ma.pedidos")
 public class MainApp {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ma/pedidos/controller/PedidosController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.ma.pedidos.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author jonatan.moreira
+ *
+ */
+@RestController
+@RequestMapping("pedidos")
+public class PedidosController {
+
+	private static final Log log = LogFactory.getLog(PedidosController.class);
+	
+	@RequestMapping("/hello")
+	@ResponseBody
+	String home() {
+		return "Hola Pedidos!!";
+	}	
+	
+}
